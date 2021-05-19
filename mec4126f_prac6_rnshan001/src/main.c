f97a0eeed01d546a6dea9957d8101812aa6dc852
Hannah Rens <rnshan001@myuct.ac.za>
2021-05-19 16:45:38 +0200

    Commit 2

--- mec4126f_prac6_rnshan001/src/main.c
+++ mec4126f_prac6_rnshan001/src/main.c
@@ -20,12 +20,15 @@ uint8_t = ADC_value; //adc value after conversation
 unsigned char buffer[16];
 
 // FUNCTION DECLARATIONS -----------------------------------------------------|
+
+void main(void);                                                   //COMPULSORY
+
 void init_ADC(void);
 void display_on_LCD();
 void init_LEDS(void);
 void display_on_LEDs();
-
-void main(void);                                                   //COMPULSORY
+void init_external_interupts(void);
+void init_buttons(void);
 
 #ifdef TRUESTUDIO												   //COMPULSORY
 	void reset_clock_to_48Mhz(void);							   //COMPULSORY
@@ -44,10 +47,13 @@ void main(void)
 	void display_on_LCD();
 	void init_LEDS();
 	void display_on_LEDs();
-	while(1)
-	{
-		ADC_value = ADC1 -> DR;
-	}
+	void display_on_LEDs();
+	void init_external_interupts();
+	void init_buttons();
+
+	while(1);
+
+
 }
 
 // OTHER FUNCTIONS -----------------------------------------------------------|
@@ -57,7 +63,7 @@ void init_ADC(void)
     RCC -> AHBENR |= RCC_AHBENR_GPIOAEN; // enable clock PORT A
     GPIOA -> MODER |= GPIO_MODER_MODER5; // set PA5, analogue mode
     ADC1 -> CFGR1 |= ADC_CFGR1_RES_1; // 8 bit resolution
-    ADC1->CFGR1 |= ADC_CFGR1_CONT; //continuous conversion mode
+    ADC1 -> CFGR1 &= ~ADC_CFGR1_CONT; //Cont = 0: single shot mode
     ADC1->CR |= ADC_CR_ADEN; //set ADEN=1
     while((ADC1->ISR & ADC_ISR_ADRDY)==0); //wait until ADRDY==1
     ADC1->CR |= ADC_CR_ADSTART; //start the ADC
@@ -86,7 +92,12 @@ void display_on_LEDs(void)
 {
     GPIOB -> ODR = ADC_value;
 }
-
+void init_buttons(void)
+{
+    RCC -> AHBENR |= RCC_AHBENR_GPIOAEN; //enable clock for push buttons
+    GPIOA -> MODER &= ~GPIO_MODER_MODER3; // configure PA0 to input mode
+    GPIOA -> PUPDR |= GPIO_PUPDR_PUPDR3_0; // pull up '01'
+}
 
 #ifdef TRUESTUDIO												   //COMPULSORY
 /* Description:
@@ -110,13 +121,28 @@ void reset_clock_to_48Mhz(void)									   //COMPULSORY
 }																   //COMPULSORY
 #endif															   //COMPULSORY
 
-
-
-
-
-
 // INTERRUPT HANDLERS --------------------------------------------------------|
-
+void init_external_interupts(void)
+{
+    RCC->APB2ENR |= RCC_APB2ENR_SYSCFGOMPEN;
+    SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PA;
+    SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI1_PA;
+    EXTI->IMR |= EXTI_IMR_MR3 | EXTI_IMR_MR1;
+    EXTI->FTSR |= EXTI_FTSR_TR3 | EXTI_FTSR_MR1;
+    NVIC_EnableIRQ(EXTI0_1_IRQn);
+}
+void EXTI0_1_IRQHandler(void)
+{
+    if (EXTI-> PR &= EXTI_PR_PR3 )// IF BUTTON IS PUSHED WAS A3
+    {
+        GPIOB->ODR = ADC_values;
+    }
+    else
+    {
+        GPIOB -> ODR = 0x00;
+    }
+    EXTI->PR |= EXTI_PR_PR3;
+}
 
 
 
